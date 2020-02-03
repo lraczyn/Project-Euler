04afd4372e26c619612a3c3b93c95f986082540f
lraczyn home <omegapoland@gmail.com>
2020-02-03 22:19:47 +0100

    euler 22

---
+++ 022.py
@@ -0,0 +1,23 @@
+from string import ascii_uppercase
+
+with open('p022_names.txt') as f:
+    names = sorted(next(f).replace('"', '').split(','))
+
+
+letters_values = {k: v for k, v in zip(ascii_uppercase, range(1, len(ascii_uppercase) + 1))}
+
+
+def calculate(names, values):
+    total_score = 0
+    position = 1
+    for name in names:
+        name_score = 0
+        for letter in name:
+            name_score += values[letter]
+        total_score += position*name_score
+        position += 1
+    return print(total_score)
+
+
+name = ['PATRICIA']
+calculate(names, letters_values)
